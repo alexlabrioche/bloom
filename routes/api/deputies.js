1edf443aeb4080e0bb0b052c70bdec8aa1982471
Vianney <vianney.guesdon.dev@gmail.com>
2019-05-14 18:38:56 +0200

    route deputies

--- routes/api/deputies.js
+++ routes/api/deputies.js
@@ -1,8 +1,12 @@
 // Require Modules
 const express = require("express");
+const slug = require("slug");
 
 // Import Model Deputy
 const Deputy = require("../../models/Deputy");
+
+// Configure slug to default url
+slug.defaults.mode = "rfc3986";
 
 // Set Router from Express
 const router = express.Router();
@@ -29,14 +33,15 @@ router.post("/add", (req, res) => {
       Deputy.countDocuments((err, count) => {
         console.log(`There are already ${count} deputies in the database`);
         const newDeputy = new Deputy({
-          // deputyNumber: count + 1,
-          name: name,
+          deputyNumber: count + 1,
+          name: req.body.name,
           participationRate: req.body.participationRate,
-          from: req.body.from,
-          to: req.body.to,
+          mandateFrom: req.body.mandateFrom,
+          mandateTo: req.body.mandateTo,
           group: req.body.group,
           party: req.body.party,
-          picture: req.body.picture
+          picture: req.body.picture,
+          slug: slug(req.body.name.toString())
         });
         newDeputy
           .save()
